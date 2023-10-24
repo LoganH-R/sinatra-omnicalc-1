8dc196b64ab258a3692389ed9bb72a396c4a0a46
LoganH-R <146144771+LoganH-R@users.noreply.github.com>
2023-10-24 02:58:38 +0000

    part of apr

--- app.rb
+++ app.rb
@@ -23,17 +23,20 @@ end
 
 get("/square_root/results") do
   @the_num = params.fetch("users_num").to_f
-  
+
   @the_result = params.fetch("users_num").to_f ** 0.5
   
   erb(:square_root_results)
 end
 
 get("/payment/new") do
-
-
-
   erb(:payment)
+end
+
+get("/payment/results") do
+  @the_apr = params.fetch("users_apr").to_f 
+  
+  erb(:payment_results)
 end
 
 get("/random/new") do
