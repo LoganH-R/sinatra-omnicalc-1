e1a28096bac7c510455e6052b6bb9c7ac394acde
LoganH-R <146144771+LoganH-R@users.noreply.github.com>
2023-10-24 17:56:52 +0000

    Couldn't round to four decimal places

--- app.rb
+++ app.rb
@@ -34,14 +34,31 @@ get("/payment/new") do
 end
 
 get("/payment/results") do
-  @the_apr = params.fetch("users_apr").to_f 
+  @the_apr = params.fetch("users_apr").to_f.to_fs(:percentage)
+  @the_years = params.fetch("users_years").to_i
+  @the_principal = params.fetch("users_principal").to_f.to_fs(:currency)
+
   
+  rate_per_period = (params.fetch("users_apr").to_f / 100) / 12
+  numerator = rate_per_period * params.fetch("users_principal").to_f
+  num_periods = @the_years * 12
+  parenthesis = 1 + rate_per_period
+  power = (parenthesis) ** -num_periods
+  denominator = 1 - power
+  @monthly_payment = (numerator / denominator).to_fs(:currency)
+    
   erb(:payment_results)
 end
 
 get("/random/new") do
-
-
-
   erb(:random)
+end
+
+get("/random/results") do
+  @the_min = params.fetch("users_min").to_f
+  @the_max = params.fetch("users_max").to_f
+
+  @the_random_num = rand(@the_min..@the_max)
+
+  erb(:random_results)
 end
